f03c4de7b0acd4fb75d1bb8fdd6e533401ea3ec0
Abel Semahegn <abel249733@gmail.com>
2025-08-03 11:12:21 -0500

    restart

--- src/db/schema.ts
+++
@@ -1,7 +0,0 @@
-import { integer, pgTable, text,date } from "drizzle-orm/pg-core";
-
-export const usersTable = pgTable("paste", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  content: text("content").notNull(), // The paste content
-  exp_date: date().notNull(),
-});
